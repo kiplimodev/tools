203c7c35b5c6f19686b977a94c3d6d37029d18b0
kiplimodev <denniskiplimo@gmail.com>
2025-12-09 19:05:08 +0200

    Implement powerlifting calculator formulas

---
+++ calories/running-calories-burned-calculator/types.ts
@@ -0,0 +1,10 @@
+export interface CalculatorInput {
+  weightKg: number;
+  durationMinutes: number;
+  speedKmh: number;
+}
+
+export interface CalculatorOutput {
+  caloriesBurned: number;
+  metValue: number;
+}
